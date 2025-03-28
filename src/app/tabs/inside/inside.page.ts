50864018fcddfa19f39f2438cbfb5f2b8d14ac71
Dominik <dasdodomit@gmail.com>
2025-03-28 19:13:25 +0100

    Removed the explorer component

--- src/app/tabs/inside/inside.page.ts
+++ src/app/tabs/inside/inside.page.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
-import { ExploreContainerComponent } from '../../explore-container/explore-container.component';
 
 @Component({
   selector: 'app-outside',
   templateUrl: 'inside.page.html',
   styleUrls: ['inside.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
 })
 export class InsidePage {
   constructor() {}
--- src/app/tabs/outside/outside.page.ts
+++ src/app/tabs/outside/outside.page.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
-import { ExploreContainerComponent } from '../../explore-container/explore-container.component';
 
 @Component({
   selector: 'app-outside',
   templateUrl: 'outside.page.html',
   styleUrls: ['outside.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
 })
 export class OutsidePage {
   constructor() {}
