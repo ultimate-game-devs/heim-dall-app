18f7e9aa6a96d99a76b9f8a243149ec76810b9c9
Dominik <dasdodomit@gmail.com>
2025-03-28 22:39:17 +0100

    Using wss

--- src/app/tabs/inside/inside.page.ts
+++ src/app/tabs/inside/inside.page.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import {MqttService} from "ngx-mqtt";
 
 @Component({
   selector: 'app-outside',
@@ -7,6 +8,15 @@ import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/stan
   styleUrls: ['inside.page.scss'],
   imports: [IonHeader, IonToolbar, IonTitle, IonContent],
 })
-export class InsidePage {
-  constructor() {}
+export class InsidePage implements OnInit {
+  constructor(private mqttService: MqttService) {}
+
+  ngOnInit() {
+    this.mqttService.observe('inside').subscribe(
+      {
+        next: (message) => {
+          console.log(message.payload.toString());
+        }
+      })
+  }
 }
