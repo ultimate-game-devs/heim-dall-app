25ef2366d59aa5207e6de6a87f96c123e1726696
Dominik <dasdodomit@gmail.com>
2025-03-28 21:13:20 +0100

    Provided mqtt

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,23 @@ import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalo
 
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
+import {IMqttServiceOptions, MqttModule} from "ngx-mqtt";
+import {importProvidersFrom} from "@angular/core";
+
+export const MQTT_SERVICE_OPTIONS: IMqttServiceOptions = {
+  hostname: '192.168.178.100',
+  port: 9001,
+  path: '/mqtt',
+  protocol: "ws"
+}
 
 bootstrapApplication(AppComponent, {
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     provideIonicAngular(),
     provideRouter(routes, withPreloading(PreloadAllModules)),
+    importProvidersFrom(
+      MqttModule.forRoot(MQTT_SERVICE_OPTIONS)
+    )
   ],
 });
