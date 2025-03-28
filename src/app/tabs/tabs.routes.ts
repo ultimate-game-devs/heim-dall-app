47026a480b433f6b0e59179f3a6e70ac770ac1d7
Dominik <dasdodomit@gmail.com>
2025-03-28 19:02:42 +0100

    Removed all other tabs than outside

--- src/app/tabs/tabs.routes.ts
+++ src/app/tabs/tabs.routes.ts
@@ -7,35 +7,20 @@ export const routes: Routes = [
     component: TabsPage,
     children: [
       {
-        path: 'tab1',
-        loadComponent: () =>
-          import('./tab1/tab1.page').then((m) => m.Tab1Page),
-      },
-      {
-        path: 'tab2',
-        loadComponent: () =>
-          import('./tab2/tab2.page').then((m) => m.Tab2Page),
-      },
-      {
-        path: 'tab3',
-        loadComponent: () =>
-          import('./tab3/tab3.page').then((m) => m.Tab3Page),
-      },
-      {
         path: 'outside',
         loadComponent: () =>
           import('./outside/outside.page').then((m) => m.OutsidePage),
       },
       {
         path: '',
-        redirectTo: '/tabs/tab1',
+        redirectTo: '/tabs/inside',
         pathMatch: 'full',
       },
     ],
   },
   {
     path: '',
-    redirectTo: '/tabs/tab1',
+    redirectTo: '/tabs/inside',
     pathMatch: 'full',
   },
 ];
